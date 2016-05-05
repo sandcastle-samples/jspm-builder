740d7a43c1b22600f5cf55b30b63236c021de2ee
Glenn Morton <glenn@sandcastle.io>
2016-05-06 00:19:00 +0800

    Initial commit

---
+++ public/app/car.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+export default class Car {
+
+  public make;
+  public model;
+
+  constructor(make: string, model: string){
+    this.make = make;
+    this.model = model;
+  }
+
+}
---
+++ public/app/index.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+import Car from './car';
+
+
+var c = new Car('toyota', 'rav4');
+
+console.log(c.make);
+
+alert('${c.make} : ${c.model}');
